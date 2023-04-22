34f892d844f63d8ae3364c1a091d0d92ef8e1856
Mateusz Turzyński <kontakt@compilatorum.pl>
2023-04-22 12:25:25 +0200

    Initial commit.

---
+++ InfoShare.Errors/InfoShare.Errors.Playground/MagicCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoShare.Errors.Playground
+{
+    internal class MagicCalculator
+    {
+        public int CalculateMagicNumber(int seed)
+        {            
+            var result = Step1(seed);
+
+            return result;
+        }
+
+        private int Step1(int seed)
+        {            
+            return seed * 2;
+        }
+    }
+}
---
+++ InfoShare.Errors/InfoShare.Errors.Playground/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace InfoShare.Errors.Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Magic calculator calculates magic number ;)");
+            Console.Write("Please enter seed number (0 - 100): ");
+
+            var seed = int.Parse(Console.ReadLine());
+
+            var magicCalculator = new MagicCalculator();
+            var result = magicCalculator.CalculateMagicNumber(seed);
+
+            Console.WriteLine($"Magic number for seed {seed} is {result}");
+        }
+    }
+}
